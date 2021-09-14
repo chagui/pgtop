2747e137a466ff50484b7c07f0dc12760bc79c49
Guilhem Charles <guilhem.charles@gmail.com>
2021-09-14 20:44:59 +0200

    remove the postfixes from CliError Enum variants

--- src/error.rs
+++ src/error.rs
@@ -3,17 +3,17 @@ use std::sync::mpsc;
 
 #[derive(Debug)]
 pub enum CliError {
-    DBError { source: tokio_postgres::Error },
-    UIError { source: io::Error },
-    PipeError { source: mpsc::RecvError },
+    DB { source: tokio_postgres::Error },
+    UI { source: io::Error },
+    Pipe { source: mpsc::RecvError },
 }
 
 impl std::fmt::Display for CliError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
-            CliError::DBError { ref source } => write!(f, "{}", source),
-            CliError::UIError { ref source } => write!(f, "{}", source),
-            CliError::PipeError { ref source } => write!(f, "{}", source),
+            CliError::DB { ref source } => write!(f, "{}", source),
+            CliError::UI { ref source } => write!(f, "{}", source),
+            CliError::Pipe { ref source } => write!(f, "{}", source),
         }
     }
 }
@@ -21,27 +21,27 @@ impl std::fmt::Display for CliError {
 impl std::error::Error for CliError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            CliError::DBError { ref source } => Some(source),
-            CliError::UIError { ref source } => Some(source),
-            CliError::PipeError { ref source } => Some(source),
+            CliError::DB { ref source } => Some(source),
+            CliError::UI { ref source } => Some(source),
+            CliError::Pipe { ref source } => Some(source),
         }
     }
 }
 
 impl From<tokio_postgres::Error> for CliError {
     fn from(err: tokio_postgres::Error) -> CliError {
-        CliError::DBError { source: err }
+        CliError::DB { source: err }
     }
 }
 
 impl From<io::Error> for CliError {
     fn from(err: io::Error) -> CliError {
-        CliError::UIError { source: err }
+        CliError::UI { source: err }
     }
 }
 
 impl From<mpsc::RecvError> for CliError {
     fn from(err: mpsc::RecvError) -> CliError {
-        CliError::PipeError { source: err }
+        CliError::Pipe { source: err }
     }
 }
